108e0692b70ff0f6466771fa81271b37868d7dd5
Zem <lourdrivera123@gmail.com>
2014-10-22 08:29:36 +0800

    added sentry and configured database

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-
+		'Cartalyst\Sentry\SentryServiceProvider',
+		'Way\Generators\GeneratorsServiceProvider',
 	),
 
 	/*
@@ -188,7 +189,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
+		'Sentry' 		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 	),
 
 );
